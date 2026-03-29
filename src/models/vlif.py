3454ee4212fdad8589515e94660b487d70476517
dangkh <dangkh109@gmail.com>
2026-03-29 13:50:29 +1100

    Update vlif.py

--- src/models/vlif.py
+++ src/models/vlif.py
@@ -575,21 +575,15 @@ class VLIF(GeneralRecommender):
         neg_item_nodes += self.n_users
 
         
-        vh_feat, self.loss_r = self.trb(self.t_feat, self.v_feat)
-
-        if self.v_feat is not None:
-            self.v_rep, self.v_preference = self.v_gcn(self.edge_index_dropv, self.edge_index, vh_feat)
-            
-        if self.t_feat is not None:
-            self.t_rep, self.t_preference = self.t_gcn(self.edge_index_dropt, self.edge_index, self.t_feat)
-            self.syn, self.s_preference = self.s_gcn(self.edge_index_dropt, self.edge_index, s_feat)
-       
+        self.v_rep, self.v_preference = self.v_gcn(self.edge_index_dropv, self.edge_index, self.v_feat)
+        
+        self.t_rep, self.t_preference = self.t_gcn(self.edge_index_dropt, self.edge_index, self.t_feat)
+    
         item_repV = self.v_rep[self.num_user:]
         item_repT = self.t_rep[self.num_user:]
-        item_s = self.syn[self.num_user:]
     
         ############################################ multi-modal information aggregation
-        item_rep = torch.cat((item_repT, item_s, item_repV), dim=1)
+        item_rep = torch.cat((item_repT, item_repV), dim=1)
         item_rep = self.item_item(item_rep)
 
 
@@ -598,12 +592,10 @@ class VLIF(GeneralRecommender):
         user_repT = self.t_rep[:self.num_user]
         user_repT = user_repT.unsqueeze(2)
 
-        user_s = self.syn[:self.num_user]
-        user_s = user_s.unsqueeze(2)
-        user_rep = torch.cat((user_repT, user_s, user_repV), dim=2)
+        user_rep = torch.cat((user_repT, user_repV), dim=2)
         user_rep = self.weight_u.transpose(1,2)*user_rep
         # add synergy
-        user_rep = torch.cat((user_rep[:,:,0], user_rep[:,:,1], user_rep[:,:,2]), dim=1)
+        user_rep = torch.cat((user_rep[:,:,0], user_rep[:,:,1]), dim=1)
 
         h_u = self.user_graph(user_rep, self.epoch_user_graph, self.user_weight_matrix)
         # comment/remove the coefficient 0.5 for cloth dataset
@@ -624,9 +616,8 @@ class VLIF(GeneralRecommender):
         loss_value = -torch.mean(torch.log2(torch.sigmoid(pos_scores - neg_scores)))
         reg_embedding_loss_v = (self.v_preference[user] ** 2).mean() if self.v_preference is not None else 0.0
         reg_embedding_loss_t = (self.t_preference[user] ** 2).mean() if self.t_preference is not None else 0.0
-        reg_embedding_loss_s = (self.s_preference[user] ** 2).mean() if self.s_preference is not None else 0.0
 
-        reg_loss = self.reg_weight * (reg_embedding_loss_v + reg_embedding_loss_t + reg_embedding_loss_s)
+        reg_loss = self.reg_weight * (reg_embedding_loss_v + reg_embedding_loss_t)
         reg_loss += self.reg_weight * (self.weight_u ** 2).mean()
         return loss_value + reg_loss
 
